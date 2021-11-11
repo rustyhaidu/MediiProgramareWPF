14c29a21e6fa52b717eaac9e5489926c7ca874f1
Claudiu <haidu_claudiu@yahoo.com>
2021-11-11 12:24:51 +0200

    rename project

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Haidu_Claudiu_Lab2
+namespace Haidu_Claudiu_Lab
 {
     /// <summary>
     /// Interaction logic for App.xaml
